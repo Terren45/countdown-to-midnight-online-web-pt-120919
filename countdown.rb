db4f0745e7c01c9b45c67353765395b7db1fe70a
Terren Culberson <culbersonterren@yahoo.com>
2019-12-14 03:33:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,4 +11,10 @@ def countdown(integer)
 end
 
 def countdown_with_sleep(integer)
-  
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    sleep(1)
+    integer -= 1
+   end
+   "HAPPY NEW YEAR!"
+end
