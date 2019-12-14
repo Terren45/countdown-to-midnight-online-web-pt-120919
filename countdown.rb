c8c44db4bfeb2b7903f73c27ca3bf4aa19412cdf
Terren Culberson <culbersonterren@yahoo.com>
2019-12-14 03:25:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,8 @@ def countdown(integer)
  #countdown to 0 
  #puts "#{integer} SECOND(S)! "
   while integer > 0
-    puts "#{integer} SECOND(S)!"  
+    puts "#{integer} SECOND(S)!" 
+    integer -= 1
    end
-   
+   "HAPPY NEW YEAR!"
 end
